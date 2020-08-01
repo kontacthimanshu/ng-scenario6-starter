5e2474a240c7e63d33168fbd39e506fbade48a4b
kontacthimanshu@hotmail.com <kontacthimanshu@hotmail.com>
2020-08-01 08:39:33 +0530

    updated styles

--- findmehome/src/app/properties/property-thumbnail.component.ts
+++ findmehome/src/app/properties/property-thumbnail.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
     .pad-left { margin-left: 10px; }
     .bold { font-weight: bold; }
     .thumbnail { min-height: 210px; padding-left: 10px; background-color: #343a40; margin-bottom:10px; }
-    .well div { color: #bbb; }
+    .well div { color: #E9E9E9; }
     .yellow {color:#FFFF00 !important}
     `]    
 })

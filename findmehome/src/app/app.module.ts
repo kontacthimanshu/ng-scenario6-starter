c840d88a848290b3fd9a230a2a7e51a785345fc0
kontacthimanshu@hotmail.com <kontacthimanshu@hotmail.com>
2020-08-15 21:19:26 +0530

    updated from scenarion 5 with latest changes

--- findmehome/src/app/app.module.ts
+++ findmehome/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 import { PropertyListComponent } from './properties/properties-list.component';
 import { PropertyThumbnailComponent } from './properties/property-thumbnail.component';
 import { NavBarComponent } from './nav/nav-bar.component';
-import { PropertiesService } from './properties/shared/properties.service';
-import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   declarations: [
@@ -17,10 +16,7 @@ import { ToastrService } from './common/toastr.service';
   imports: [
     BrowserModule
   ],
-  providers: [
-    PropertiesService,
-    ToastrService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- findmehome/src/app/properties/property-thumbnail.component.ts
+++ findmehome/src/app/properties/property-thumbnail.component.ts
@@ -6,9 +6,13 @@ import { Component, Input } from '@angular/core';
     styles: [`
     .pad-left { margin-left: 10px; }
     .bold { font-weight: bold; }
-    .thumbnail { min-height: 210px; padding-left: 10px; background-color: #343a40; margin-bottom:10px; }
-    .well div { color: #E9E9E9; }
-    .yellow {color:#FFFF00 !important}
+    .thumbnail { min-height: 325px; padding: 20px 25px; background-color: #fff; margin-bottom:10px; color: #000; border: 1px solid #ccc;
+        border-radius: 10px;
+        -moz-box-shadow: 3px 3px 5px 3px #ccc;
+        -webkit-box-shadow: 3px 3px 5px 3px #ccc;
+        box-shadow: 3px 3px 5px 3px #ccc;}
+    .prop-details div { color: #000; font-weight:700;  }
+    .green {color:#1aac2d !important}
     `]    
 })
 export class PropertyThumbnailComponent
